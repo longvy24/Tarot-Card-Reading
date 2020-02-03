2d7d3507db068f3cac17428af19703db25e86736
Long <36736540+longvy24@users.noreply.github.com>
2020-02-03 11:45:32 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -18,6 +18,74 @@ int past,
     rerollpresent,
     rerollfuture;
 
+char *foolpast =  "In the Past position could represent a time in the past "
+                  " where you could have taken another route " 
+                  "and ended up somewhere different than where you are now. "
+                  "It could also mean that you have already set things "
+                  "into motion that will produce a new path for the person in question. It can also represent time wasted.",
+
+      *foolpre = "In the present position places the person in the mind of the fool but " 
+                 "in their current position.They could be about to abandon something, or "
+                 " are looking for new horizons and wanting to enter a new phase of life. "
+                 "Generally, you may feel as though are about to begin a new journey. ",
+
+      *foolfut = "In the future position relates to, unsurprisingly, a future journey. "
+                 "Not a literal journey where you pack things up, but a personal and spiritual "
+                 "journey. New goals, desires, and attainment of new knowledge await. "
+                 "The Fool is especially good to draw for the future if you are starting a new " 
+                 "school, workplace or other activity that will be a big part of your life.",
+
+      *magipast = "In the past position shows you have achieved something in the past, whether you "
+                  "feel the achievement or not doesn’t matter, but you are still benefiting from it. "
+                  "The past is the best predictor of the future, are you have set yourself up well "
+                  "through a possible number of means; good grades, good relationships, or good experience.",
+      
+      *magipre  = " In the present position represents your ambition and confidence. "
+                  " You might have just started a project or a new job. You may have new ways to help people, "
+                  "through either learning from your elders, or helping those that you can. Consider this transfer of knowledge.",
+    
+      *magifut  = "In the future position represents your potential. As said above, "
+                  "The Magician is a good card to draw. Maintain your vision and see "
+                  "it through. Don’t get distracted by side projects, and don’t think you know everything just because you’ve started.",
+                
+      *hippast  = "In the past position shows a time in the past where you have had to be patient "
+                  "to proceed. Maybe all that time studying a dull subject, or waiting in a "
+                  "long queue for an important matter.",
+
+      *hippre   = "In the present position suggests you should avoid conflict "
+                  "and action and ‘mull over’ the things you’re being told. They may just be an "
+                  "illusion, or, someone is telling you one thing but hinting on the bigger picture. ",
+
+      *hipfut   = "In the future position represents possibly coming required patience or the "
+                  "unveiling of a secret that you might not be privy too, or will overhear.",
+      *emprpast =
+      *emprpre  =
+      *emprfut  =
+      *empepast =
+      *empepre  =
+      *empefut  =
+      *hiepast  =
+      *hiepre   =
+      *hiefut   =
+      *lovpast  =
+      *lovpre   =
+      *lovfut   =
+      *chapast  =
+      *chapre   =
+      *chafut   =
+      *strpast  =
+      *strpre   =
+      *strfut   =
+      *herpast  =
+      *herpre   =
+      *herfut   =
+      *
+      *
+      *
+      *
+      *
+      *
+      
 
 int main ()
 {
@@ -26,6 +94,8 @@ srand((unsigned)time(0));
 past = rand()%22+1;
 present = rand()%22+1;
 future = rand()%22+1;
+        
+        
 
 /* This is test script for the random numbers and their rerolls
 cout << past << endl;
